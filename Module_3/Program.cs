8d8215127bb6bf5f93326e20a15585e5a3666579
TatarPrincess <TatarPrincessP@gmail.com>
2022-09-20 14:05:03 +0300

    Unit 1

---
+++ Module_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName, hello, age;
+        myName = "\t My name is Ksusha \n";
+        hello = "\t Hello, world! \n";
+        age = "\t I'm 38";
+
+        Console.WriteLine(hello + myName + age);
+
+        Console.ReadKey();
+    }
+}
