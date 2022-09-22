92ae6b907fd92ab60fb903a8f0c8c16ba9112167
TatarPrincess <TatarPrincessP@gmail.com>
2022-09-22 14:38:25 +0300

    Unit 3. Final task

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -12,18 +12,25 @@ class MainClass
         Saturday = 6,
         Sunday = 7
     };
-    
+
 
     public static void Main(string[] args)
     {
+        //name
         Console.WriteLine("Enter your name: ");
         string name = Console.ReadLine();
+        //age
         Console.WriteLine("Enter your age: ");
         byte age = Convert.ToByte(Console.ReadLine());
-        Console.WriteLine("Your name is {0} and your age is {1} ", name, age);
+        //favorite day
         Console.WriteLine("What is your favorite day of week? ");
         byte favDayAnswer = Convert.ToByte(Console.ReadLine());
         string favDayAnswerStr = Enum.GetName(typeof(DaysOfWeek), favDayAnswer);
-        Console.WriteLine("Your favorite day is {0}", favDayAnswerStr);
+        //birthday
+        Console.WriteLine("Enter your date of birth: ");
+        string dateOfBirth = Console.ReadLine();
+
+        Console.WriteLine("\tYour name is {0}\n \tYour age is {1}\n \tYour birthday is {2}\n \tYour favorite day is {3}\n", name, age, dateOfBirth, favDayAnswerStr);
+
     }
 }
