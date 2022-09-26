bc253c52b38dc4ef81cc32088067e5fa582136a5
TatarPrincess <TatarPrincessP@gmail.com>
2022-09-26 14:43:09 +0300

    Task 4.1.17

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -2,35 +2,34 @@
 
 class MainClass
 {
-    enum DaysOfWeek : byte
-    {
-        Monday = 1,
-        Tuesday = 2,
-        Wednesday = 3,
-        Thursday = 4,
-        Friday = 5,
-        Saturday = 6,
-        Sunday = 7
-    };
-
-
     public static void Main(string[] args)
     {
-        //name
-        Console.WriteLine("Enter your name: ");
-        string name = Console.ReadLine();
-        //age
-        Console.WriteLine("Enter your age: ");
-        byte age = Convert.ToByte(Console.ReadLine());
-        //favorite day
-        Console.WriteLine("What is your favorite day of week? ");
-        byte favDayAnswer = Convert.ToByte(Console.ReadLine());
-        string favDayAnswerStr = Enum.GetName(typeof(DaysOfWeek), favDayAnswer);
-        //birthday
-        Console.WriteLine("Enter your date of birth: ");
-        string dateOfBirth = Console.ReadLine();
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        Console.WriteLine("\tYour name is {0}\n \tYour age is {1}\n \tYour birthday is {2}\n \tYour favorite day is {3}\n", name, age, dateOfBirth, favDayAnswerStr);
+        var color = Console.ReadLine();
 
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine($"Your color is {color}!");
+                break;
+        }
     }
 }
