08150e5e2498027d91f017d83a4ed4b52f5fa2c4
TatarPrincess <TatarPrincessP@gmail.com>
2022-10-03 12:00:59 +0300

    task 4.3.17

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -6,25 +6,29 @@ class MainClass
     public static void Main(string[] args)
     {
         int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-        int quant = 0;
+        int a = 0;
 
-        for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+        for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)   //перебираем строки 
         {
-            for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
+            for (int k = 0; k < arr.GetUpperBound(1) + 1; k++) //для каждой строки перебираем столбцы
             {
-                if (arr[i,k] > 0)
+                for (int m = k+1; m < arr.GetUpperBound(1) + 1; m++)
                 {
-                    quant++;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-        }
-        
-        Console.WriteLine(quant);
-        
+                    if (arr[i, k] > arr[i, m])
+                    {
+                        a = arr[i, k];
+                        arr[i, k] = arr[i, m];
+                        arr[i, m] = a;
+                        
+                    }
+                    else { continue; }
+                    
 
+                }
+                Console.Write(arr[i,k] + "  ");
+            }
+            Console.WriteLine();
+        }     
+        
     }
 }
