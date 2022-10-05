ddc1becf2cbd369124df567651887ad1646c0668
TatarPrincess <TatarPrincessP@gmail.com>
2022-10-05 13:48:31 +0300

    5.3.1

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -6,46 +6,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        var arr = getArrayFromConsole();
-        var sortedArr = sortArray(arr);
-        
-        
-        foreach (var item in sortedArr)
-        {
-            Console.WriteLine(item);
-        };
+        var myAge = 20;
+        Console.WriteLine("Before - {0}", myAge);
+        changeAge(ref myAge);
 
-
-    }
-    static int[] getArrayFromConsole()
-    {
-        var result = new int[5];
-        for (int i = 0; i < result.Length; i++)
+        static void changeAge(ref int myAge)
         {
-            Console.WriteLine("Введите элемент массива номер {0}", i+1);
-            result[i] = Convert.ToInt32(Console.ReadLine());
+            myAge = 38;
+            Console.WriteLine("In the method - {0}", myAge);
         }
-        return result;
-    }
-    static int[] sortArray(string name, params int[] array)
-    {
-        var b = 0;
-        Console.WriteLine(name);
 
-        for (int x = 0; x < array.Length; x++)
-        {
-            for (int y = x + 1; y < array.Length; y++)
-            {
-                if (array[x] > array[y])
-                {
-                    b = array[x];
-                    array[x] = array[y];
-                    array[y] = b;
-                }
-                else continue;
-            }
-        }
-        return array;
+        Console.WriteLine("After - {0}", myAge);
     }
-
 }
