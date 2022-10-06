71ec5a632821583c1239bbd5ee4187ccb12acad2
TatarPrincess <TatarPrincessP@gmail.com>
2022-10-06 15:06:06 +0300

    task 5.5.4

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -4,96 +4,37 @@ using System.Drawing;
 
 class MainClass
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        int size = 3;
-        var arr = getArrayFromConsole(ref size);
-        Console.WriteLine();
-        sortArray(out int[] sorteddesc, out int[] sortedasc, arr);
+        Console.WriteLine("Напишите что-то");
+        var str = Console.ReadLine();
 
-        foreach (int item in sorteddesc)
-        {
-            Console.Write(item + " ");
-        }
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
 
-        Console.WriteLine();
+        Echo(str, deep);
 
-        foreach (int item in sortedasc)
-        {
-            Console.Write(item + " ");
-        }
-    }
-    static int[] getArrayFromConsole(ref int size)
-    {
-        size += 3;
-        var result = new int[size];
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.WriteLine("Введите элемент массива номер {0}", i+1);
-            result[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        return result;
-    }
-    static void sortArray(out int[] sorteddesc, out int[] sortedasc, in int[] array)
-    {
-        sorteddesc = sortArrayDesc(array);
-        sortedasc = sortArrayAsc(array);
-              
-    }
-    static int[] sortArrayAsc(in int[] array)
-    {
-        var b = 0;        
-
-        for (int x = 0; x < array.Length; x++)
-        {
-            for (int y = x + 1; y < array.Length; y++)
-            {
-                if (array[x] > array[y])
-                {
-                    b = array[x];
-                    array[x] = array[y];
-                    array[y] = b;
-                }
-                else continue;
-            }
-        }
-
-        int[] arraySortedAsc = new int[6];
-
-        for (int i = 0; i < array.Length; i++)
-        {
-            arraySortedAsc[i] = array[i];        
-        }
-
-        return arraySortedAsc;
+        Console.ReadKey();
     }
 
-    static int[] sortArrayDesc(in int[] array)
+    static void Echo(string saidWord, int deep)
     {
-        var b = 0;
+        var modif = saidWord;
 
-        for (int x = 0; x < array.Length; x++)
+        if (modif.Length > 2)
         {
-            for (int y = x + 1; y < array.Length; y++)
-            {
-                if (array[x] < array[y])
-                {
-                    b = array[x];
-                    array[x] = array[y];
-                    array[y] = b;
-                }
-                else continue;
-            }
+            modif = modif.Remove(0, 2);
         }
 
-        int[] arraySortedDesc = new int[6];
+        var color = (ConsoleColor) deep; 
+        Console.ForegroundColor = color;
+        Console.WriteLine("..." + saidWord);
+        
 
-        for (int i = 0; i < array.Length; i++)
+
+        if (deep > 1)
         {
-            arraySortedDesc[i] = array[i];
+            Echo(modif, deep - 1);
         }
-
-        return arraySortedDesc;
-
     }
 }
