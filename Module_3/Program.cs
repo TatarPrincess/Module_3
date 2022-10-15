f6c2bd9450961e36b77f00c3b9ee6363eee2585b
TatarPrincess <TatarPrincessP@gmail.com>
2022-10-15 10:57:05 +0300

    6.2.2

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -1,218 +1,93 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Drawing;
+using System.Globalization;
 
 class Programm
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        (string name,
-         string surname,
-         int age,
-         bool hasPet,
-         int petQuant,
-         string[] petNames,
-         string[] favColors) anketa;
+        //var department = GetCurrentDepartment();
+        Console.WriteLine("введите количество пассажиров");
+        int result;
+        int.TryParse(Console.ReadLine(), out result);
+        int? quant = result;
+        if (result <= 0) quant = null;
+        Bus bus = new Bus(quant);
 
-        string [] getPetNameArray (int quant)
-        {            
-           Console.WriteLine("Перечислите имена питомцев через запятую ");
-           string answer = Console.ReadLine();
-           string [] arr = new string[quant];
-           return answer.Split(',');             
-            
-        }
-
-        string[] getfavColors(int quant)
-        {
-            if (quant > 0)
-            {
-                Console.WriteLine("Перечислите любимые цвета через запятую ");
-                string answerColor = Console.ReadLine();
-                anketa.favColors = new string[quant];
-                return answerColor.Split(','); ;
-            }
-            else
-            {
-                var emptyArr = new string[] {""};
-                return emptyArr;
-            }
-           
-        }
-
-        bool isCorrectInput(string input, string type)
-        {
-            int tmp;
-            if (!int.TryParse(input, out tmp)) return false;
-
-            switch (type)
-            {
-                case "age":
-                    {
-                        if (tmp > 0 && tmp < 110) return true;
-                        else return false;                        
-                    }
-                    
-                case "pet":
-                    {
-                        if (tmp > 0) return true;
-                        else return false;
-                    }
-                    
-                case "color":
-                    {
-                        if (tmp >= 0) return true;
-                        else return false;
-                    }
-                    
-                default: return false;
-            }
-            
-            
-        }
-
-        void getName ()
-        {
-            Console.WriteLine("Ведите свое имя");
-            string myName = Console.ReadLine();
-            if (myName.Length > 0)
-            {
-                anketa.name = myName;
-            }
-            else
-            {
-                Console.WriteLine("Не верно введено имя");
-                getName();
-            } 
-        };
-
-        void getSurName()
-        {
-            Console.WriteLine("Ведите свою фамилию");
-            string mySurName = Console.ReadLine();
-            if (mySurName.Length > 0)
-            {
-                anketa.surname = mySurName;
-            }
-            else
-            {
-                Console.WriteLine("Не верно введена фамилия");
-                getSurName();
-
-            } 
-        };
-
-        void getAge()
-        {
-            Console.WriteLine("Ведите свой возраст");
-            string myAge = Console.ReadLine();
-
-            if (isCorrectInput(myAge, "age"))
-            {
-                anketa.age = Int32.Parse(myAge);                
-            }
-            else
-            {
-                Console.WriteLine("Не верно введен возраст");
-                getAge();
-            } 
-        };
-
-        void getPetInfo()
-        {
-            Console.WriteLine("Есть ли у вас питомец? (ответьте да или нет)");
-            string havePet = Console.ReadLine();
-            anketa.petQuant = 0;
-            anketa.petNames = null;
-
-            switch (havePet)
-            {
-                case "да":
-                    {
-                        anketa.hasPet = true;
-                        Console.WriteLine("Сколько у вас питомцев? ");
-                        string myPetQuant = Console.ReadLine();
-                        if (isCorrectInput(myPetQuant, "pet"))
-                        {
-                            anketa.petQuant = Int32.Parse(myPetQuant);
-                            
-                        }
-
-                        if (anketa.petQuant > 0)
-                        {
-                            anketa.petNames = getPetNameArray(anketa.petQuant);
-                        }
-                        break;
-                    }
-                case "нет":
-                    {
-                        anketa.hasPet = false;
-                        anketa.petQuant = 0;
-                        break;
-                    }  
-                default:
-                    {
-                        Console.WriteLine("Не верный ответ на вопрос. Есть ли у вас питомцы?");
-                        getPetInfo();
-                        break;
-                    } 
-            }
-        };
-
-        void getColorInfo()
-        {
-            Console.WriteLine("Введите количество любимых цветов");
-            string myColorQuant = Console.ReadLine();
-
-            if (isCorrectInput(myColorQuant, "color"))
-            {
-                anketa.favColors = getfavColors(int.Parse(myColorQuant));
-            }
-            else 
-            {
-                Console.WriteLine("Не верный ответ на вопрос. Введите количество любимых цветов?");
-                getColorInfo();
-            }
-            
-        };
-
-        (string name, string surname, int age, bool hasPet, int petQuant, string[] petNames, string[] favColors) fillAnketa()
-        {
-            getName();
-            getSurName();
-            getAge();
-            getPetInfo();
-            getColorInfo();
-            return anketa;
-        };
-
-        void showAnketa((string name, string surname, int age, bool hasPet, int petQuant, string[] petNames, string[] favColors) myAnketa)
-        {
-            Console.WriteLine("Ваше имя: {0}", myAnketa.name);
-            Console.WriteLine("Ваша фамилия: {0}", myAnketa.surname);
-            Console.WriteLine("Ваш возраст: {0}", myAnketa.age);
-            string myHasPet;
-            if (myAnketa.hasPet == true)
-            {
-                myHasPet = "Да"; 
-            } else myHasPet = "Нет";
-            Console.WriteLine("Есть ли у вас домашние животные: {0}", myHasPet);
-            Console.WriteLine("Клички домашних животных в количестве {0}: ", anketa.petQuant);
-            foreach (string item in myAnketa.petNames)
-            {
-                Console.WriteLine(item + ""); 
-            }
-            Console.WriteLine("Ваши любимые цвета: ");
-            foreach (string item in myAnketa.favColors)
-            {
-                Console.WriteLine(item + "");
-            }
-        };
-
-        showAnketa(fillAnketa());
-        
+        bus.PrintStatus();
     }
 
+    static Department GetCurrentDepartment()
+    {
+        Console.WriteLine("Введите название города");
+        City city = new City(Console.ReadLine());
+        Console.WriteLine("Введите тип компании");
+        string compType = Console.ReadLine();
+        Console.WriteLine("Введите имя компании");
+        string compName = Console.ReadLine();
+        Company company = new Company(compType, compName);
 
+        if (company.Type == "Банк" && city.Name == "Санкт-Петербург")
+        {
+            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", company.Name);
+        }
+        else Console.WriteLine("ничего");
+
+        return new Department(company, city);
+    }
 
 }
+class Bus
+{
+    public int? Load;
+    public Bus(int? load)
+    {
+        Load = load;
+    }
+
+    public void PrintStatus()
+    {
+        if (Load != null)
+        {
+            Console.WriteLine("Количество пассажиров - {0}", Load);
+        }
+        else Console.WriteLine("Автобус пуст");
+
+    }
+}
+class Company
+{
+    public string Type;
+    public string Name;
+    public Company(string type, string name = "Неизвестная компания")
+    {
+        Type = type;
+        Name = name;
+    }
+}
+
+class City
+{
+    public string Name;
+    public City(string name)
+    {
+        Name = name;    
+    }
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+
+    public Department(Company compData, City cityData)
+    {
+        Company = compData;
+        City = cityData;
+    }
+
+    
+
+}
+
