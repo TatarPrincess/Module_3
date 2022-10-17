ba2463ac1978ab7a3d470c774939fb918da64cba
TatarPrincess <TatarPrincessP@gmail.com>
2022-10-17 19:28:49 +0300

    6.5.2

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -7,87 +7,74 @@ class Programm
 {
     static void Main(string[] args)
     {
-        //var department = GetCurrentDepartment();
-        Console.WriteLine("введите количество пассажиров");
-        int result;
-        int.TryParse(Console.ReadLine(), out result);
-        int? quant = result;
-        if (result <= 0) quant = null;
-        Bus bus = new Bus(quant);
 
-        bus.PrintStatus();
     }
 
-    static Department GetCurrentDepartment()
+    class User
     {
-        Console.WriteLine("Введите название города");
-        City city = new City(Console.ReadLine());
-        Console.WriteLine("Введите тип компании");
-        string compType = Console.ReadLine();
-        Console.WriteLine("Введите имя компании");
-        string compName = Console.ReadLine();
-        Company company = new Company(compType, compName);
+        private int age;
+        private string login;
+        private string email;
 
-        if (company.Type == "Банк" && city.Name == "Санкт-Петербург")
+        public int Age
         {
-            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", company.Name);
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
         }
-        else Console.WriteLine("ничего");
 
-        return new Department(company, city);
-    }
-
-}
-class Bus
-{
-    public int? Load;
-    public Bus(int? load)
-    {
-        Load = load;
-    }
-
-    public void PrintStatus()
-    {
-        if (Load != null)
+        public string Login
         {
-            Console.WriteLine("Количество пассажиров - {0}", Load);
+            get 
+            {
+                return login;
+            }
+            set
+            {
+                if (value.Length >= 3)
+                {
+                    login = value;
+                }
+                else Console.WriteLine("Длина поля логин должна быть более 3 символов длиной");
+                
+            }
         }
-        else Console.WriteLine("Автобус пуст");
 
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if (value.Contains('@'))
+                {
+                    email = value;
+                }
+                else Console.WriteLine("Email должен содержать символ @");
+
+            }
+
+        }
     }
 }
-class Company
-{
-    public string Type;
-    public string Name;
-    public Company(string type, string name = "Неизвестная компания")
-    {
-        Type = type;
-        Name = name;
-    }
-}
 
-class City
-{
-    public string Name;
-    public City(string name)
-    {
-        Name = name;    
-    }
-}
 
-class Department
-{
-    public Company Company;
-    public City City;
 
-    public Department(Company compData, City cityData)
-    {
-        Company = compData;
-        City = cityData;
-    }
 
-    
-
-}
 
