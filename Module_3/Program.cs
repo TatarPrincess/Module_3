7be0d36fe5c3452ebeaf3094d0c5c938956803f3
TatarPrincess <TatarPrincessP@gmail.com>
2022-11-08 22:40:41 +0300

    8.4.3

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -1,46 +1,45 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Linq;
+
+[Serializable]
+class Contact
+{
+    public string Name { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string name, long phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+}
 
 class Programme
 {
     static void Main(string[] args)
     {
-        string path = @"C:/Users/xiaomi/Desktop/BinaryFile.bin";
-        
-        WriteString(path);
-        ReadString(path);
-    }
+        Contact contact = new Contact("Ivan", 89263453245, "ivan@gmail.com");
+        Console.WriteLine("Object is created");
 
-    static void WriteString(string path)
-    {
-        FileInfo fileInfo = new FileInfo(path);
-        Stream stream = fileInfo.Open(FileMode.Open);
-        if (fileInfo.Exists)
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = @"C:\Store\serObj.dat";
+        using (var stream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
         {
-            using (BinaryWriter writer = new BinaryWriter(stream))
-            {
-                DateTime now = DateTime.Now;
-                OperatingSystem os = Environment.OSVersion;
-                string strToWrite = "Файл изменен " + now + "на компьютере " + os;
-                writer.Write(strToWrite);   
+            formatter.Serialize(stream, contact);
+            Console.WriteLine("Object is serialized");
+        }
 
-            }
+        using (var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+        {
+            var newContact = (Contact)formatter.Deserialize(stream);
+            Console.WriteLine("Object is deserialized");
         }
     }
 
-    static void ReadString(string path)
-    {
-        FileInfo fileInfo = new FileInfo(path);
-        Stream stream = fileInfo.Open(FileMode.Open);
-        if (fileInfo.Exists)
-        {
-            using (BinaryReader reader = new BinaryReader(stream))
-            {
-                string str = reader.ReadString();
-                Console.WriteLine(str);
-
-            }
-        }
-    }
+    
 }
 
 
